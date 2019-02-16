746262cba29b01431b7e0b317b2347f784a74da7
Brandon Chen <brandon.chen@stonybrook.edu>
2019-02-16 15:19:14 -0500

    Added output string

--- ttt/app.js
+++ ttt/app.js
@@ -1,21 +1,27 @@
-const express = require('express')
-const app = express()
-const port = 80
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 80;
 
-app.get('/ttt', (req, res) => res.sendFile('index.html', { root: __dirname}))
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+app.get('/ttt', (req, res) => res.sendFile('index.html', { root: __dirname}));
+
 app.get('/css/styles.css', function(req, res) {
-  res.sendFile("css/styles.css", { root: __dirname})
-})
+  res.sendFile('css/styles.css', { root: __dirname})
+});
 
+app.post('/ttt', urlencodedParser, function (req, res) {
+  if (!req.body) return res.sendStatus(400);
+  console.log(`Hello ${req.destroy.name}, $` + getDate());
+});
 
-app.post('/ttt', (req, res) => {
-  console.log(req)
-})
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
-
-
-
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-
-
+function getDate() {
+  var date = new Date();
+  var year = date.getFullYear();
+  var month = (date.getMonth() + 1 < 10) ? ('0' + (date.getMonth() + 1)) : date.getMonth() + 1;
+  var day = (date.getDate() < 10) ? ('0' + date.getDate()) : date.getDate();
+  return year + '-' + month + '-' + day;
+}
