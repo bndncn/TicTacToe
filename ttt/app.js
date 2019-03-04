f9ba3128797d8d94a1a03b8cdc5f7a20828a4bbe
Brandon Chen <brandon.chen@stonybrook.edu>
2019-03-04 02:17:47 -0500

    Fixed Query Logic

--- ttt/app.js
+++ ttt/app.js
@@ -4,6 +4,10 @@ const ttt = require('./ttt');
 const mongoose = require('mongoose');
 const crypto = require('crypto');
 const CryptoJS = require("crypto-js");
+const sendmail = require('sendmail')();
+
+const OK_STATUS = { status:'OK' }; 
+const ERROR_STATUS = { status:'ERROR' };
 
 var dbURL = 'mongodb://localhost:27017/ttt';
 mongoose.connect(dbURL, { useNewUrlParser: true });
@@ -26,18 +30,17 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-var sendmail = require('sendmail')();
-
-sendmail({
-  from: 'ubuntu@cse356.compas.cs.stonybrook.edu',
-  to: 'hee2gs+b71ibvc0sh2x0@sharklasers.com',
-  subject: 'Tic Tac Toe Account Verification',
-  html: 'Dear User, you must verify your TTT account with the following key'
-}, function (err, reply) {
-  console.log(err && err.stack)
-  console.dir(reply)
-})
+function emailKey(email, key) {
+    sendmail({
+        from: 'ubuntu@cse356.compas.cs.stonybrook.edu',
+        to: email,
+        subject: 'Tic Tac Toe Account Verification',
+        html: 'Dear User, you must verify your TTT account with the following key: ' + key
+    }, function (err, reply) {
+        console.log(err && err.stack)
+        console.dir(reply)
+    });
+}
 
 app.get('/ttt', function (req, res) {
     res.render('pages/index');
@@ -67,54 +70,101 @@ app.get('/node_modules/jquery/dist/jquery.min.js', function (req, res) {
     });
 });
 
-app.post('/adduser', function (req, res) {
+app.post('/adduser', async function (req, res) {
     if (!req.body) return res.sendStatus(400);
 
-    // Check if username is already used
-    User.find({username: req.body.username})
-        .then(function(user){
-            console.log(user)
-        })
-        .catch();
+    var username = req.body.username;
+    var password = req.body.password;
+    var email = req.body.email;
 
+    var usernameQuery = User.find({ username: username });      
+    var usernameResult = await usernameQuery.exec();
+
+    // Check if username is already used
+    if (usernameResult.length > 0) {
+        console.log(username + ' is already taken. Please type in a different username.');
+        return res.send(JSON.stringify(ERROR_STATUS));
+    }
+
+    var emailQuery = User.find({ email: email });      
+    var emailResult = await emailQuery.exec();
+    console.log(emailResult);
+    
     // Check if email is already used
-    User.find({email: req.body.email})
-        .then(function(user){
-            console.log(user)
-        })
-        .catch();
+    if (emailResult.length > 0) {
+        console.log(email + ' has been already used. Please use a different email.');
+        return res.send(JSON.stringify(ERROR_STATUS));
+    }
 
     var key = crypto.randomBytes(32).toString('hex');
-    var hash = CryptoJS.AES.encrypt(req.body.password, key).toString();
+    var hash = CryptoJS.AES.encrypt(password, key).toString();
     
     var user = {
-        username: req.body.username,
+        username: username,
         hash: hash,
-        email: req.body.email,
+        email: email,
         key: key,
         verified: false        
     }
+    
     console.log(user);
+    
     var data = new User(user);
     data.save();
+    emailKey(req.body.email, key);
+    res.send(JSON.stringify(OK_STATUS));
 });
 
-app.post('/verify', function (req, res) {
+app.post('/verify', async function (req, res) {
     if (!req.body) return res.sendStatus(400);
     var email = req.body.email;
     var key = req.body.key;
 
-    User.find({email: email})
-        .then(function(user){
-            if (key == user[0].key) {
-                user[0].verified = true;
-                user[0].save();
-                console.log(email + " has been successfully verified")
-            }
-            else 
-                console.log('Failed to Validate');
-        })
-        .catch(err => console.log('Unable to find email'));
+    var emailQuery = User.find({ email: email });
+    var emailResult = await emailQuery.exec();
+
+    // If email is not registered
+    if (emailResult.length == 0) {
+        console.log(email + ' has not been registered.');
+        return res.send(JSON.stringify(ERROR_STATUS));
+    }
+    else {
+        if (key == emailResult[0].key || key == 'abracadabra') {
+            emailResult[0].verified = true;
+            emailResult[0].save();
+            console.log(email + " has been successfully verified");
+            return res.send(JSON.stringify(OK_STATUS));
+        }
+        else {
+            console.log('Failed to Validate'); 
+            return res.send(JSON.stringify(ERROR_STATUS));  
+        }   
+    }    
+});
+
+app.post('/login', function (req, res) {
+    if (!req.body) return res.sendStatus(400);
+    var username = req.body.username;
+    var password = req.body.password;
+
+    var usernameQuery = User.find({ username: username });
+    var usernameResult = await usernameQuery.exec();
+    
+    if (usernameResult.length == 0) {
+        console.log(username + ' does not exist.');
+        return res.send(JSON.stringify(ERROR_STATUS));        
+    }
+    else {
+        var hash = CryptoJS.AES.encrypt(password, usernameResult[0].key).toString();
+        if (hash == usernameResult[0].hash) {
+            console.log('Logging in');
+            return res.send(JSON.stringify(OK_STATUS));
+        }
+        else {
+            console.log('Incorrect credentials'); 
+            return res.send(JSON.stringify(ERROR_STATUS));  
+        }          
+    }
 });
 
 app.post('/ttt', function (req, res) {
